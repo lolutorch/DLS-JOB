68092331e1f064abd4ac025eb0c073e29dec8206
lolutorch <109870019+lolutorch@users.noreply.github.com>
2022-09-06 17:19:54 -0400

    Create dsl.groovy

---
+++ dsl.groovy
@@ -0,0 +1,19 @@
+job('NodeJS example') {
+    scm {
+       git('https://github.com/namanjainyr/ChatApp.git') { node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigNamef ('Naman Jain')
+            node / gitConfigEmaiK ('namanjainyr@gmail.com')
+       }
+    }
+    triggers {
+     scm('H/5 * * * *')
+    }
+    wrappers {
+      nodejs('nodejs')  // this is the name of the NodeJS installation in
+                        // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
+    steps {
+        shell("npm install")
+    }
+}
+
